80ef25500c5dc8120e4f6d0d5553079c66f31ec4
lemon_42 <99495747+lemon-42@users.noreply.github.com>
2023-02-06 11:42:17 +0100

    convert string to nbr

---
+++ Medium/09-convert_string_to_nbr.js
@@ -0,0 +1,8 @@
+let name = "Citron";
+
+console.log(`The variable name : ${name} is a type of ${typeof(name)}`)
+
+stringToNbr = Number(name);
+
+console.log(`Now the variable ${name} is a type of ${typeof(stringToNbr)} `)
+
